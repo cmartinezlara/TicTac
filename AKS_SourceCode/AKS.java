d7bfbd6fcf8d24edf306b86b566bd5aec21cfbb5
manumg201 <100405917@alumnos.uc3m.es>
2022-03-10 16:31:27 +0100

    T(n)

--- AKS_SourceCode/AKS.java
+++ AKS_SourceCode/AKS.java
@@ -74,72 +74,76 @@ public class AKS extends Thread
 		// TODO: Do this in linear time http://www.ams.org/journals/mcom/1998-67-223/S0025-5718-98-00952-1/S0025-5718-98-00952-1.pdf
 		// If ( n = a^b for a in natural numbers and b > 1), output COMPOSITE
 		long t1 = System.nanoTime();
-		BigInteger base = BigInteger.valueOf(2);
-		BigInteger aSquared;
+		BigInteger base = BigInteger.valueOf(2); // 1
+		BigInteger aSquared;// 1
 		
 		do
 		{
-			BigInteger result;
+			BigInteger result; // 1 
 
-			int power = Math.max((int) (log()/log(base) - 2),1);
-			int comparison;
+			int power = Math.max((int) (log()/log(base) - 2),1); // 2 + 13 + 8
+			int comparison; // 1
 			
 			do
 			{
-				power++;
-				result = base.pow(power);
-				comparison = n.compareTo(result);
+				power++; // 1
+				result = base.pow(power); // 2 
+				comparison = n.compareTo(result); // 2 
 			}
-			while( comparison > 0 && power < Integer.MAX_VALUE );
+			while( comparison > 0 && power < Integer.MAX_VALUE ); //3 veces + 1 --> 4 * (2 + 5) = 28 
 			
-			if( comparison == 0 )
+			if( comparison == 0 ) // 1 + "lo de dentro" = 1 + 5 = 6
 			{
-				if (verbose) System.out.println(n + " is a perfect power of " + base);
-				factor = base;
-				n_isprime = false;
-				return n_isprime;
+				if (verbose) System.out.println(n + " is a perfect power of " + base); // 2 
+				factor = base; // 1
+				n_isprime = false; // 1
+				return n_isprime; // 1
 			}
 			
-			if (verbose) System.out.println(n + " is not a perfect power of " + base);
+			if (verbose) System.out.println(n + " is not a perfect power of " + base); // 2
 
-			base = base.add(BigInteger.ONE);
-			aSquared = base.pow(2);
+			base = base.add(BigInteger.ONE); // 2
+			aSquared = base.pow(2); // 2
 		}
-		while (aSquared.compareTo(this.n) <= 0);
-		if (verbose) System.out.println(n + " is not a perfect power of any integer less than its square root");
+		while (aSquared.compareTo(this.n) <= 0); // floor(raiz(n)) - 1 + 1 --> floor(raiz(n)) * (2 + 65) = floor(raiz(n))*67
+		if (verbose) System.out.println(n + " is not a perfect power of any integer less than its square root"); // 2
         
+		//--- Total P1: floor(raiz(n))*67 + 4; Potencial
+
+
 		//-----PASO 2-----
 		// Find the smallest r such that o_r(n) > log^2 n
 		// o_r(n) is the multiplicative order of n modulo r
 		// the multiplicative order of n modulo r is the 
 		// smallest positive integer k with	n^k = 1 (mod r).
-		double log = this.log();
-		double logSquared = log*log;
-		BigInteger k = BigInteger.ONE;
-		BigInteger r = BigInteger.ONE;
+		double log = this.log(); // 1 + 13
+		double logSquared = log*log; // 2 
+		BigInteger k = BigInteger.ONE; // 1
+		BigInteger r = BigInteger.ONE; // 2
 		do
 		{
-			r = r.add(BigInteger.ONE);
-			if (verbose) System.out.println("trying r = " + r);
-			k = multiplicativeOrder(r);
+			r = r.add(BigInteger.ONE); // 2
+			if (verbose) System.out.println("trying r = " + r); // 1+1
+			k = multiplicativeOrder(r); // 8r+8
 		}
-		while( k.doubleValue() < logSquared );
-		if (verbose) System.out.println("r is " + r);
+		while( k.doubleValue() < logSquared ); // r * (2 + 8r+12) = 8r^2 + 14r
+		if (verbose) System.out.println("r is " + r); // 2
 		
+		//---- Total P2: 8r^2 + 14r +21 donde r = max(3, log(n)^5)
 		//-----PASO 3-----
 		// If 1 < gcd(a,n) < n for some a <= r, output COMPOSITE
-		for( BigInteger i = BigInteger.valueOf(2); i.compareTo(r) <= 0; i = i.add(BigInteger.ONE) )
+		for( BigInteger i = BigInteger.valueOf(2); i.compareTo(r) <= 0; i = i.add(BigInteger.ONE) ) //5 +  (r-1)*(5 + 4)
 		{
-			BigInteger gcd = n.gcd(i);
-			if (verbose) System.out.println("gcd(" + n + "," + i + ") = " + gcd);
-			if ( gcd.compareTo(BigInteger.ONE) > 0 && gcd.compareTo(n) < 0 )
+			BigInteger gcd = n.gcd(i);// 1 
+			if (verbose) System.out.println("gcd(" + n + "," + i + ") = " + gcd); // 2 
+			if ( gcd.compareTo(BigInteger.ONE) > 0 && gcd.compareTo(n) < 0 ) // 1  
 			{
-				factor = i;
-				n_isprime = false;
-				return false;
+				factor = i; // 1
+				n_isprime = false; // 1
+				return false; // 1
 			}
 		}
-		
+		 // ---Total P3: (r-1)*9 + 5
 		//if (salir) return true; //CORTE PARA LAS PRUEBAS
 		
 		//-----PASO 4-----
@@ -152,7 +156,7 @@ public class AKS extends Thread
 
 		//-----PASO 5-----
 		// For i = 1 to sqrt(totient)log(n) do
-		// if (X+i)^n <> X^n + i (mod X^r - 1,n), output composite;
+		// if (X+i)^n <>ï¿½X^n + i (mod X^r - 1,n), output composite;
 		long t2 = System.nanoTime();
 		// sqrt(totient)log(n)
 		int limit = (int) (Math.sqrt(totient(r).doubleValue()) * this.log());
@@ -225,26 +229,26 @@ public class AKS extends Thread
 	 * @param r modulus for mutliplicative order
 	 * @return multiplicative order or -1 if none exists
 	 */
-	BigInteger multiplicativeOrder(BigInteger r)
+	BigInteger multiplicativeOrder(BigInteger r)// 8r + 7
 	{
 		// TODO Consider implementing an alternative algorithm http://rosettacode.org/wiki/Multiplicative_order
-		BigInteger k = BigInteger.ZERO;
-		BigInteger result;
+		BigInteger k = BigInteger.ZERO; // 1
+		BigInteger result; // 1
 		
 		do
 		{
-			k = k.add(BigInteger.ONE);
-			result = this.n.modPow(k,r);
+			k = k.add(BigInteger.ONE); // 2 
+			result = this.n.modPow(k,r); // 2
 			//System.out.println("k: " + k + " r: " + r);
 		}
-		while( result.compareTo(BigInteger.ONE) != 0 && r.compareTo(k) > 0);
+		while( result.compareTo(BigInteger.ONE) != 0 && r.compareTo(k) > 0); // r * (4 + 4)
 		
-		if (r.compareTo(k) <= 0)
-			return BigInteger.ONE.negate();
-		else
+		if (r.compareTo(k) <= 0) // 2 + 3
+			return BigInteger.ONE.negate(); // 1
+		else 
 		{
-			if (verbose) System.out.println(n + "^" + k + " mod " + r + " = " + result);
-			return k;
+			if (verbose) System.out.println(n + "^" + k + " mod " + r + " = " + result); // 1 + 1
+			return k; // 1
 		}
 	}
 	
@@ -256,24 +260,24 @@ public class AKS extends Thread
 	 * 
 	 * @return log base 2 of n
 	 */
-	double log()
+	double log() // 13
 	{
-		if ( logSave != -1 )
+		if ( logSave != -1 ) // 1 
 			return logSave;
 		
 		// from http://world.std.com/~reinhold/BigNumCalcSource/BigNumCalc.java
-		BigInteger b;
+		BigInteger b; //  1
 		
-	    int temp = n.bitLength() - 1000;
-	    if (temp > 0) 
+	    int temp = n.bitLength() - 1000; //3
+	    if (temp > 0) // 1 + 6
 	    {
-	    	b=n.shiftRight(temp); 
-	        logSave = (Math.log(b.doubleValue()) + temp)*Math.log(2);
+	    	b=n.shiftRight(temp);  // 2
+	        logSave = (Math.log(b.doubleValue()) + temp)*Math.log(2); // 4
 	    }
 	    else 
-	    	logSave = (Math.log(n.doubleValue()))*Math.log(2);
+	    	logSave = (Math.log(n.doubleValue()))*Math.log(2); // 4
 
-	    return logSave;
+	    return logSave; // 1
 	}
 
 	
@@ -282,19 +286,19 @@ public class AKS extends Thread
 	 * @param x
 	 * @return
 	 */
-	double log(BigInteger x)
+	double log(BigInteger x)// 8
 	{
 		// from http://world.std.com/~reinhold/BigNumCalcSource/BigNumCalc.java
-		BigInteger b;
+		BigInteger b; // 1
 		
-	    int temp = x.bitLength() - 1000;
-	    if (temp > 0) 
+	    int temp = x.bitLength() - 1000; // 3
+	    if (temp > 0) // 1 + 5
 	    {
-	    	b=x.shiftRight(temp); 
-	        return (Math.log(b.doubleValue()) + temp)*Math.log(2);
+	    	b=x.shiftRight(temp); // 2
+	        return (Math.log(b.doubleValue()) + temp)*Math.log(2);  // 3
 	    }
 	    else 
-	    	return (Math.log(x.doubleValue())*Math.log(2));
+	    	return (Math.log(x.doubleValue())*Math.log(2)); // 3
 	}
 	
 	public BigInteger getFactor()
